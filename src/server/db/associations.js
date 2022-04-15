cb6be9428a2789a6d67d9553ef0f69971e6bd74c
Lenin Garizabalo <lcamargo@protonmail.com>
2022-04-15 17:09:40 -0500

    add associations file

--- src/server/db/associations.js
+++ src/server/db/associations.js
@@ -1,3 +1,11 @@
-import { sequelize } from './database';
+const associations = (sequelize) => {
+	const { Products, Users, Carts } = sequelize.models;
+	// product | cart
+	Products.belongsToMany(Carts, { through: "products_carts" });
+	Carts.belongsToMany(Products, { through: "products_carts" });
+	// user | cart
+	Users.belongsToMany(Carts, { through: "users_carts" });
+	Carts.belongsTo(Users, { through: "users_carts" });
+};
 
-const { Product } = sequelize.models;
+export default associations;
