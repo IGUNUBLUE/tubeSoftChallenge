455747edb3da757bde97fc3f99cb8f42c1e64494
Lenin Garizabalo <lcamargo@protonmail.com>
2022-04-15 20:02:57 -0500

    add user seeder

--- src/server/db/seeders/20220415203945-demo-users.js
+++ src/server/db/seeders/20220415203945-demo-users.js
@@ -1,24 +1,41 @@
-'use strict';
+// eslint-disable-next-line strict
+"use strict";
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+	async up(queryInterface) {
+		await queryInterface.bulkInsert(
+			"users",
+			[
+				{
+					name: "John",
+					lastName: "Doe",
+					email: "jhon.doe@challenge.tubesoft",
+					password: "1a2b3c",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					name: "Tomas",
+					lastName: "Morales",
+					email: "tomas.morales@challenge.tubesoft",
+					password: "1122aabb",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+				{
+					name: "Lenin",
+					lastName: "Garizablo",
+					email: "lenin.doe@challenge.tubesoft",
+					password: "1a2b3c",
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				},
+			],
+			{}
+		);
+	},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete("People", null, {});
+	},
 };
