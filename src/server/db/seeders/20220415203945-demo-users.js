93b866c5eb51c1a608bada890c554d6de953572c
Lenin Garizabalo <lcamargo@protonmail.com>
2022-04-15 20:38:46 -0500

    fix seeder

--- src/server/db/seeders/20220415203945-demo-users.js
+++ src/server/db/seeders/20220415203945-demo-users.js
@@ -36,6 +36,6 @@ module.exports = {
 	},
 
 	async down(queryInterface, Sequelize) {
-		await queryInterface.bulkDelete("People", null, {});
+		await queryInterface.bulkDelete("users", null, {});
 	},
 };
